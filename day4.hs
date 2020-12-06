8b45445a5aa2eefa3b96f417ebacc86c2ccb53be
Olivier Mehani <olivier.mehani@learnosity.com>
2020-12-07 10:54:07 +1100

    day4 PartI: isValidPassport
    Signed-off-by: Olivier Mehani <olivier.mehani@learnosity.com>

--- day4.hs
+++ day4.hs
@@ -1,9 +1,78 @@
 import System.IO
 
+type PassportField = String
+
 main = do
   input <- getContents
-  let passports = lines2 input
-  print passports
+  let passports = map (makePassport) $ lines2 input
+  print $ length $ filter (isValidPassport) passports
+
+-- | 'makePassport' take a string description of a passport, and returns a Passport
+--
+-- >>> makePassport "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm"
+-- ["ecl:gry","pid:860033327","eyr:2020","hcl:#fffffd","byr:1937","iyr:2017","cid:147","hgt:183cm"]
+--
+-- >>> makePassport "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\nhcl:#cfa07d byr:1929"
+-- ["iyr:2013","ecl:amb","cid:350","eyr:2023","pid:028048884","hcl:#cfa07d","byr:1929"]
+--
+-- >>> makePassport "hcl:#ae17e1 iyr:2013\neyr:2024\necl:brn pid:760753108 byr:1931\nhgt:179cm"
+-- ["hcl:#ae17e1","iyr:2013","eyr:2024","ecl:brn","pid:760753108","byr:1931","hgt:179cm"]
+--
+-- >>> makePassport "hcl:#cfa07d eyr:2025 pid:166559648\niyr:2011 ecl:brn hgt:59in"
+-- ["hcl:#cfa07d","eyr:2025","pid:166559648","iyr:2011","ecl:brn","hgt:59in"]
+--
+makePassport :: String -> [PassportField]
+makePassport s = words $ unlines $ lines s
+
+-- | 'isValidPassport' verifies that a passport is valid, in that it contains
+-- only (?)
+-- - byr (Birth Year)
+-- - iyr (Issue Year)
+-- - eyr (Expiration Year)
+-- - hgt (Height)
+-- - hcl (Hair Color)
+-- - ecl (Eye Color)
+-- - pid (Passport ID)
+-- - cid (Country ID)
+-- with cid being optional
+--
+-- >>> isValidPassport ["ecl:gry","pid:860033327","eyr:2020","hcl:#fffffd","byr:1937","iyr:2017","cid:147","hgt:183cm"]
+-- True
+--
+-- >>> isValidPassport ["iyr:2013","ecl:amb","cid:350","eyr:2023","pid:028048884","hcl:#cfa07d","byr:1929"]
+-- False
+--
+-- Treat missing cid as valid
+-- >>> isValidPassport ["hcl:#ae17e1","iyr:2013","eyr:2024","ecl:brn","pid:760753108","byr:1931","hgt:179cm"]
+-- True
+--
+-- >>> isValidPassport ["hcl:#cfa07d","eyr:2025","pid:166559648","iyr:2011","ecl:brn","hgt:59in"]
+-- False
+--
+isValidPassport :: [PassportField] -> Bool
+isValidPassport p = (length (filter (isValidPassportField) p)) == 7
+
+-- | 'isValidPassportField' verifies that a passport field is valid, in that it starts with
+-- either
+-- - byr (Birth Year)
+-- - iyr (Issue Year)
+-- - eyr (Expiration Year)
+-- - hgt (Height)
+-- - hcl (Hair Color)
+-- - ecl (Eye Color)
+-- - pid (Passport ID)
+-- with cid being optional
+isValidPassportField :: PassportField -> Bool
+isValidPassportField pf = case take 4 pf of "byr:" -> True
+                                            "iyr:" -> True
+                                            "eyr:" -> True
+                                            "hgt:" -> True
+                                            "hcl:" -> True
+                                            "ecl:" -> True
+                                            "pid:" -> True
+                                            -- "cid:" -> True
+                                            otherwise -> False
+
 
 -- | 'lines2' breaks a string up into a list of strings at newline
 -- characters.  The resulting strings do not contain newlines2.
