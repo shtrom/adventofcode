ed0e73839687e0404db52b2c0d98646932f2a9cb
Olivier Mehani <olivier.mehani@learnosity.com>
2021-12-01 17:10:10 +1100

    2021 day 1.2
    Signed-off-by: Olivier Mehani <olivier.mehani@learnosity.com>

--- 2021/day1.hs
+++ 2021/day1.hs
@@ -4,8 +4,12 @@ main = do
         input <- getContents
         let measurements = makeInts $ lines input
         let differences = makeDiffs measurements
-        let drops = length $ filter (<1) differences
+        let drops = length $ filter (<0) differences
         print drops
+        let groups = groupSums $ makeGroups measurements
+        let groupDiff = makeDiffs groups
+        let raises = length $ filter (<0) groupDiff
+        print raises
 
 -- | Make a list of Ints from a list of Strings
 -- >>> makeInts ["1", "2"]
@@ -30,3 +34,25 @@ makeInt s = read s :: Int
 makeDiffs :: Num a => [a] -> [a]
 makeDiffs [] = []
 makeDiffs (x:xs) = zipWith (-) (x:xs) xs
+
+-- | Make consecutive groups of three int
+-- >>> makeGroups []
+-- []
+-- >>> makeGroups [1]
+-- []
+-- >>> makeGroups [1,2]
+-- []
+-- >>> makeGroups [1,2,3]
+-- [(1,2,3)]
+-- >>> makeGroups [1,2,3,4]
+-- [(1,2,3),(2,3,4)]
+-- >>> makeGroups [1,2,3,4,5]
+-- [(1,2,3),(2,3,4),(3,4,5)]
+makeGroups :: [a] -> [(a, a, a)]
+makeGroups l = zip3 l (drop 1 l) (drop 2 l)
+
+-- | Make sums from triples
+-- >>> groupSums [(1,2,3),(2,3,4),(3,4,5)]
+-- [6,9,12]
+groupSums :: Num a => [(a,a,a)] -> [a]
+groupSums l= [x+y+z | (x,y,z) <- l]
