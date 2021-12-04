1235eebf875491852103e355a6e817ee97c34037
Olivier Mehani <shtrom@ssji.net>
2021-12-04 17:31:12 +1100

    2021 Day 3.2
    Signed-off-by: Olivier Mehani <shtrom@ssji.net>

--- 2021/day3.hs
+++ 2021/day3.hs
@@ -3,7 +3,7 @@ import System.IO
 main = do
         input <- getContents
         print $ day31 $ lines input
-        -- print $ day32 $ lines input
+        print $ day32 $ lines input
 
 -- | Day 3 part 1
 -- >>> day31 ["00100","11110","10110","10111","10101","01111","00111","11100","10000","11001","00010","01010"]
@@ -15,20 +15,31 @@ day31 l = let bitCounts = countBits $ transpose $ map splitToInts l
           in gamma*epsilon
 
 -- | Day 3 part 2
--- > >> day32 ["00100","11110","10110","10111","10101","01111","00111","11100","10000","11001","00010","01010"]
+-- >>> day32 ["00100","11110","10110","10111","10101","01111","00111","11100","10000","11001","00010","01010"]
 -- 230
--- day32 :: Num a => [String] -> a
--- day32 l = let matrix = map splitToInts l
---               co2 = selectRow (mostCommon) $ countBits1 $ map head matrix
---               o2 = selectRow (leastCommon) $ countBits1 $ map head matrix
---           in co2*o2
+day32 :: [String] -> Int
+day32 l = let matrix = map splitToInts l
+              co2 = bitsToDec $ selectRow (mostCommon) matrix
+              o2 = bitsToDec $ selectRow (leastCommon) matrix
+          in co2*o2
 
 -- | Recusively filter rows based on the bit criterion
+-- >>> selectRow (mostCommon) []
+-- []
+-- >>> selectRow (mostCommon) [[]]
+-- []
+-- >>> selectRow (mostCommon) [[1]]
+-- [1]
+-- >>> selectRow (mostCommon) [[1,0],[1,1]]
+-- [1,1]
 -- >>> selectRow (mostCommon) [[0,0,1],[0,1,0],[0,1,1]]
 -- [0,1,1]
 selectRow :: ((Int,Int) -> Int) -> [[Int]] -> [Int]
+selectRow _ [] = []
+selectRow _ [l] = l
 selectRow f m = let bitCriterion = f $ countBits1 $ map (head) m
-                in [bitCriterion]
+                 in (bitCriterion : (selectRow f $
+                         [xs | (x:xs) <- m, x == bitCriterion] ))
 
 -- | Splint a string of digits into an array of ints
 -- >>> splitToInts "01189991819992453"
@@ -61,10 +72,12 @@ countBits1 ll = (
   )
 
 -- | Find the most common bit value from a bitcount
--- >>> mostCommon(1,2)
+-- >>> mostCommon (1,2)
 -- 1
--- >>> mostCommon(2,1)
+-- >>> mostCommon (2,1)
 -- 0
+-- >>> mostCommon (1,1)
+-- 1
 mostCommon :: (Int,Int) -> Int
 mostCommon (z, o) = case (z>o) of
                        True -> 0
