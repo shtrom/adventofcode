fcb55fbac40c1f8eda20559629f84adc2e1a56ea
Olivier Mehani <olivier.mehani@learnosity.com>
2021-12-06 18:33:54 +1100

    2021: Split AoCUtils
    Signed-off-by: Olivier Mehani <olivier.mehani@learnosity.com>

---
+++ 2021/AoCUtils.hs
@@ -0,0 +1,24 @@
+module AoCUtils (
+        splitStringAt
+        ,toInt
+                ) where
+
+--d | Split String at arbitrary character matching predicate
+-- >>> splitStringAt (==',') "1,2,3"
+-- ["1","2","3"]
+splitStringAt :: (Char -> Bool) -> String -> [String]
+splitStringAt _ "" = [""]
+splitStringAt p s = let (xs, ys) = break p s
+          in case ys of
+             [] -> [xs]
+             [y] -> [xs, [y]]
+             otherwise -> (xs
+                          : splitStringAt p (tail ys)
+                          )
+
+-- | Convert string to an Int
+-- >>> toInt "2"
+-- 2
+toInt :: String -> Int
+toInt s = read s ::Int
+
--- 2021/day6.hs
+++ 2021/day6.hs
@@ -1,29 +1,12 @@
 import System.IO
+import AoCUtils
 
 data LanternFish = LanternFish Int deriving Show
 
 main = do
         line <- getContents
         print $ day6 80 $ map toInt $ splitStringAt (==',') line
-
---d | Split String at arbitrary character matching predicate
--- >>> splitStringAt (==',') "1,2,3"
--- ["1","2","3"]
-splitStringAt :: (Char -> Bool) -> String -> [String]
-splitStringAt _ "" = [""]
-splitStringAt p s = let (xs, ys) = break p s
-          in case ys of
-             [] -> [xs]
-             [y] -> [xs, [y]]
-             otherwise -> (xs
-                          : splitStringAt p (tail ys)
-                          )
-
--- | Convert string to an Int
--- >>> toInt "2"
--- 2
-toInt :: String -> Int
-toInt s = read s ::Int
+        print $ day6 256 $ map toInt $ splitStringAt (==',') line
 
 -- | Day 6 test
 -- >>> day6 80 [3,4,3,1,2]
