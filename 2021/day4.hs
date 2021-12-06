8a376958caebe7c99e883591304bb8fa88b61032
Olivier Mehani <olivier.mehani@learnosity.com>
2021-12-06 18:45:35 +1100

    2021 day4 some work on parsing
    Signed-off-by: Olivier Mehani <olivier.mehani@learnosity.com>

--- 2021/day4.hs
+++ 2021/day4.hs
@@ -1,11 +1,14 @@
 import System.IO
+import AoCUtils
 
-type Number = Int
-type Sheet = [Number]
+data Number = Number Int
+            | Marked deriving Show
+type Sheet = [[Number]]
 
 main = do
         input <- getContents
         print input
+        -- print $ day41 $ parseInput input
 
 -- | Parse an input string into a series of numbers and sheets
 -- >>> parseInput "1,2,3\n\n1  2  3\n1  2  3\n\n3  4  5\n3  4  5\n"
@@ -46,14 +49,20 @@ break2 p l@(x1:x2:xs) = case and [p x1, p x2] of
 -- >>> parseNumbers "1,2,3"
 -- [1,2,3]
 parseNumbers :: String -> [Number]
-parseNumbers _ = []
+parseNumbers x = map toNumber $ splitStringAt (==',') x
+
+-- | Parse an input string into a numbers
+-- >>> parseNumbers "1"
+-- 1
+toNumber :: String -> Number
+toNumber s = Number $ toInt s
 
 -- | Parse a list of input strings into a series of sheets
 -- >>> parseSheets ["1  2  3\n1  2  3", "3  4  5\n3  4  5\n"]
 -- [[[1,2,3],[1,2,3]],[[3,4,5],[3,4,5]]]
 parseSheets :: [String] -> [Sheet]
 parseSheets l = map parseSheet l
---
+
 -- | Parse an input string into a sheet
 -- >>> parseSheet "1  2  3\n1  2  3"
 -- [[1,2,3],[1,2,3]]
