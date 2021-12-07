946e9643fcf0296e7f4afe9b520c59e85659f363
Olivier Mehani <olivier.mehani@learnosity.com>
2021-12-07 17:49:41 +1100

    [2021] day7 part 2
    Signed-off-by: Olivier Mehani <olivier.mehani@learnosity.com>

--- 2021/day7.hs
+++ 2021/day7.hs
@@ -4,6 +4,7 @@ import AoCUtils
 main = do
         line <- getContents
         print $ day71 $ map toInt $ splitStringAt (==',') $ head $ lines line
+        print $ day72 $ map toInt $ splitStringAt (==',') $ head $ lines line
 
 -- | Day7 part 1
 -- >>> day71 [16,1,2,0,4,2,7,1,2,14]
@@ -16,3 +17,21 @@ day71 l = sum $ map (abs . ((-) $ median l)) l
 -- 3
 median :: [Int] -> Int
 median l = head $ drop (div (length l) 2) $ sort l
+
+-- | Day7 part 2
+-- >>> day72 [16,1,2,0,4,2,7,1,2,14]
+-- 168
+day72 :: [Int] -> Int
+day72 l = sum $ map (fuelConsumption . abs . ((-) $ mean l)) l
+
+-- | Calculate an integral mean, almost correctly
+-- >>> mean [1,2,8,10,25]
+-- 9
+mean :: [Int] -> Int
+mean l = round $ realToFrac ((fromIntegral $ sum l) / (fromIntegral $ length l))
+
+-- | Calculate the fuel consumption for a given movement
+-- >>> fuelConsumption 11
+-- 66
+fuelConsumption :: Int -> Int
+fuelConsumption n = sum $ [1..n]
