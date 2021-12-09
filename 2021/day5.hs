8dc8014e3e5b32862428984652195129b214fd30
Olivier Mehani <olivier.mehani@learnosity.com>
2021-12-09 16:07:00 +1100

    [2021] day5 intersection wip
    Signed-off-by: Olivier Mehani <olivier.mehani@learnosity.com>

--- 2021/day5.hs
+++ 2021/day5.hs
@@ -38,3 +38,27 @@ parseSegment s = let (ss:es:cruft) = splitStringAt (=='-') $ filter (not. flip e
 parseCoords :: String -> Coords
 parseCoords s = let (x:y:[]) = map toInt $ splitStringAt (==',') s
                 in (x, y)
+
+-- | Determine if two segments intersect
+-- >>> segmentIntersect ((0,0),(0,1)) ((5,0),(5,1))
+-- False
+-- >>> segmentIntersect ((5,3),(5,6)) ((3,5),(6,5))
+-- True
+-- >>> segmentIntersect ((0,0),(5,5)) ((0,5),(5,0))
+-- True
+segmentIntersect :: Segment -> Segment -> Bool
+segmentIntersect ((x11, y11),(x12,y12)) ((x21, y21),(x22,y22)) = False
+-- https://algotree.org/algorithms/computational_geometry/line_segment_intersection/
+-- segmentIntersect ((x11, y11),(x12,y12)) ((x21, y21),(x22,y22)) = let or11 = orientation ((x11, y11),(x12,y12)) ((x12,y12),(x21, y21))
+--                                                                      or12 = orientation ((x11, y11),(x12,y12)) ((x12,y12),(x22, y22))
+--                                                                      or21 = orientation ((x11, y11),(x12,y12)) ((x12,y12),(x21, y21))
+--                                                                      or22 = orientation ((x11, y11),(x12,y12)) ((x12,y12),(x22, y22))
+--                                                                 in or11 /= or12
+--                                                                 && or21 /= or22
+
+orientation s1 s2 = let alpha = slope s1
+                        beta = slope s2
+                     in alpha - beta
+
+slope :: Segment -> Int
+slope ((x1, y1),(x2,y2)) = div (y2-y1) (x2-x1)
