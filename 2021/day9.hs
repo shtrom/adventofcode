3d839f1f7c793ccbb7499bccb67474ed1c00a273
Olivier Mehani <shtrom@ssji.net>
2021-12-09 23:16:27 +1100

    [2021] day9 crawling ok, but redundant calls
    Signed-off-by: Olivier Mehani <shtrom@ssji.net>

--- 2021/day9.hs
+++ 2021/day9.hs
@@ -13,33 +13,109 @@ day91 l = findLowPoints l
 
 -- | Find low points in a 2D matrix
 -- >>> findLowPoints ["219", "398","985"]
--- [1,5]
-findLowPoints :: Ord a => [[a]] -> [a]
+-- "15"
+findLowPoints :: (Bounded a, Ord a) => [[a]] -> [a]
 findLowPoints s = findLowPoints' [] [] s
 
 
 -- | Find low points given a north and west row, and the rest of a matrix
 -- >>> findLowPoints' [] []  ["219", "398","985"]
--- [1,5]
+-- "15"
 -- >>> findLowPoints' "19" "39" ["98", "85"]
--- [5]
-findLowPoints' :: Ord a => [a] -> [a] -> [[a]] -> [a]
+-- "5"
+-- >>> findLowPoints' "9" "9" ["8", "5"]
+-- "5"
+-- >>> findLowPoints' "98" "9" ["85"]
+-- "5"
+findLowPoints' :: (Bounded a, Ord a) => [a] -> [a] -> [[a]] -> [a]
+findLowPoints' _ _ [] = []
+findLowPoints' _ _ [[]] = []
 findLowPoints' ns ws l@(x:xs) = let (n, e, s, w, p) = getPoints ns ws l
+                                    (ens, ews, el) = getNextEast ns ws l
+                                    (sns, sws, sl) = getNextSouth ns ws l
+                                    elp = findLowPoints' ens ews el
+                                    slp = findLowPoints' sns sws sl
                                  in case isLowPoint n e s w p of
-                                    True -> [p]
-                                    False -> []
+                                    True -> [p] ++ elp ++ slp
+                                    False -> [] ++ elp ++ slp
 
 -- | Extract the points, wich sensible defaults
--- >>> getPoints [1,2] [3,4] [[5,6],[7,8]]
+-- >>> getPoints [1,2] [3,4] [[5,6],[7,8 :: Int]]
 -- (1,6,7,3,5)
-getPoints :: [a] -> [a] -> [[a]] -> (a, a, a, a, a)
+-- >>> getPoints [] [] [[5,6],[7,8 :: Int]]
+-- (9223372036854775807,6,7,9223372036854775807,5)
+-- >>> getPoints [] []  ["219", "398","985"]
+-- ('\1114111','1','3','\1114111','2')
+-- >>> getPoints "98" "9" ["85"]
+-- ('9','5','\1114111','9','8')
+-- >>> getPoints "8" "8" ["5"]
+-- ('8','\1114111','\1114111','8','5')
+-- >>> getPoints "85" "9" []
+-- *** Exception: nothing left to extract
+-- ...
+getPoints :: Bounded a => [a] -> [a] -> [[a]] -> (a, a, a, a, a)
 getPoints _ _ [] = error "nothing left to extract"
-getPoints ns ws (x:xs) = let p = head x
-                             n = head ns
-                             e = head (drop 1 x)
-                             s = head (head xs)
-                             w = head ws
+getPoints ns ws (x:xs) = let p = case x of
+                                   [] -> maxBound
+                                   _ -> head x
+                             n = case ns of
+                                   [] -> maxBound
+                                   otherwise -> head ns
+                             e = case x of
+                                   [] -> maxBound
+                                   (y:[]) -> maxBound
+                                   (y:ys) -> head ys
+                             s = case xs of
+                                   [] -> maxBound
+                                   (y:ys) -> head y
+                             w = case ws of
+                                   [] -> maxBound
+                                   otherwise -> head ws
                            in (n, e, s, w, p)
+
+-- | Shift the data east
+-- >>> getNextEast [] []  ["219", "398","985"]
+-- ("","239",["19","98","85"])
+-- >>> getNextEast [] "198"  ["9", "8","5"]
+-- ("","985",[])
+-- >>> getNextEast "" "985" []
+-- ("","",[])
+getNextEast:: Bounded a => [a] -> [a] -> [[a]] -> ([a], [a], [[a]])
+getNextEast _ _ [] = ([],[],[])
+getNextEast ns ws l = let ens = case ns of
+                                  [] -> []
+                                  _ -> tail ns
+                          ews = case l of
+                                  [] -> []
+                                  ([]:_) -> []
+                                  _ -> map head l
+                          el = case l of
+                                 [] -> []
+                                 _ -> filter (not . empty) $ map tail l
+                       in (ens, ews, el)
+
+empty :: Foldable t => t a -> Bool
+empty x = length x == 0
+
+-- | Shift the data south
+-- >>> getNextSouth [] []  ["219", "398","985"]
+-- ("219","",["398","985"])
+-- >>> getNextSouth "398" []  ["985"]
+-- ("985","",[])
+-- >>> getNextSouth "985" "" []
+-- ("","",[])
+getNextSouth:: Bounded a => [a] -> [a] -> [[a]] -> ([a], [a], [[a]])
+getNextSouth _ _ [] = ([],[],[])
+getNextSouth ns ws l = let sns = case l of
+                                  [] -> []
+                                  _ -> head l
+                           sws = case ws of
+                                  [] -> []
+                                  _ -> tail ws
+                           sl = case l of
+                                 [] -> []
+                                 _ -> tail l
+                         in (sns, sws, sl)
 
 -- | Determine if a point is the lowest of its vicinity
 -- >>> isLowPoint 9 9 9 9 1
@@ -51,6 +127,12 @@ getPoints ns ws (x:xs) = let p = head x
 -- >>> isLowPoint 9 9 1 9 5
 -- False
 -- >>> isLowPoint 9 9 9 1 5
+-- False
+-- >>> isLowPoint '\1114111' '1' '3' '\1114111' '2'
+-- False
+-- >>> isLowPoint '\1114111' '4' '3' '\1114111' '2'
+-- True
+-- >>> isLowPoint 9 9 9 9 9
 -- False
 isLowPoint :: Ord a => a -> a -> a -> a -> a -> Bool
 isLowPoint n e s w p = all (>p) [n,e,s,w]
