e68a071c98decf4772c554880a9b04fec97a4f2f
Olivier Mehani <shtrom@ssji.net>
2021-12-17 00:08:26 +1100

    [2021] day 15 part 1 memoised, but not any better
    Signed-off-by: Olivier Mehani <shtrom@ssji.net>

--- 2021/day15.hs
+++ 2021/day15.hs
@@ -5,13 +5,23 @@ type Risk = Int
 type Cave = ([Risk], [Risk], [[Risk]])
 
 main = do input <- getContents
-          let cave = parseCave $ map (map (\x -> read [x] ::Int)) $ lines input
-          print $ day151 cave
+          let cave =  map (map (\x -> read [x] ::Int)) $ lines input
+              parsedCave = parseCave cave
+          -- print $ day151 parsedCave
+          -- print cave
+          print $ day151' cave
 
 -- | Find the shortest path in the cave
--- >>> day151([1,6,3,7,5,1,7,4,2],[2,3,7,1,1,3,1,2],[[3,8,1,3,7,3,6,7,2],[1,3,6,5,1,1,3,2,8],[6,9,4,9,3,1,5,6,9],[4,6,3,4,1,7,1,1,1],[3,1,9,1,2,8,1,3,7],[3,5,9,9,1,2,4,2,1],[1,2,5,4,2,1,6,3,9],[2,9,3,1,3,8,5,2,1],[3,1,1,9,4,4,5,8,1]])
+-- >>> day151 ([1,6,3,7,5,1,7,4,2],[2,3,7,1,1,3,1,2],[[3,8,1,3,7,3,6,7,2],[1,3,6,5,1,1,3,2,8],[6,9,4,9,3,1,5,6,9],[4,6,3,4,1,7,1,1,1],[3,1,9,1,2,8,1,3,7],[3,5,9,9,1,2,4,2,1],[1,2,5,4,2,1,6,3,9],[2,9,3,1,3,8,5,2,1],[3,1,1,9,4,4,5,8,1]])
 -- 40
 day151 = sum . lowestRiskPath
+
+-- | Find the shortest path in the cave
+-- >>> day151' [[1,1,6,3,7,5,1,7,4,2],[1,3,8,1,3,7,3,6,7,2],[2,1,3,6,5,1,1,3,2,8],[3,6,9,4,9,3,1,5,6,9],[7,4,6,3,4,1,7,1,1,1],[1,3,1,9,1,2,8,1,3,7],[1,3,5,9,9,1,2,4,2,1],[3,1,2,5,4,2,1,6,3,9],[1,2,9,3,1,3,8,5,2,1],[2,3,1,1,9,4,4,5,8,1]]
+-- 40
+day151' c = let w = length $ head c
+                h = length c
+             in sum $ lowestRiskPath' c (w-1) (h-1)
 
 -- | Parse a cave into up, left and rest components
 -- >>> parseCave [[1,1,6,3,7,5,1,7,4,2],[1,3,8,1,3,7,3,6,7,2],[2,1,3,6,5,1,1,3,2,8],[3,6,9,4,9,3,1,5,6,9],[7,4,6,3,4,1,7,1,1,1],[1,3,1,9,1,2,8,1,3,7],[1,3,5,9,9,1,2,4,2,1],[3,1,2,5,4,2,1,6,3,9],[1,2,9,3,1,3,8,5,2,1],[2,3,1,1,9,4,4,5,8,1]]
@@ -22,7 +32,47 @@ parseCave (x:xs) = let up = tail x
                        rest = map tail xs
                     in (up, left, rest)
 
+-- | From [0]
+-- [0] https://stackoverflow.com/a/5553390/10660788
+memo :: (Int -> Int -> a) -> [[a]]
+memo f = map (\x -> map (f x) [0..]) [0..]
+
+pathStore :: [[Risk]] -> [[[Risk]]]
+pathStore c = memo (lowestRiskPath' c)
+
+fastLowestRiskPath :: [[Risk]] -> Int -> Int -> [Risk]
+fastLowestRiskPath c x y = pathStore c !! x !! y
+
+-- | Memoised version of lowestRiskPath'
+-- >>> lowestRiskPath' [[1,2,3],[4,5,6],[7,8,9]] 0 0
+-- []
+-- >>> lowestRiskPath' [[1,2,3],[4,5,6],[7,8,9]] 1 1
+-- [6,9]
+-- >>> lowestRiskPath' [[1,2,3],[4,5,6],[7,8,9]] 2 2
+-- [2,3,6,9]
+lowestRiskPath' :: [[Risk]] -> Int -> Int -> [Risk]
+lowestRiskPath' c 0 0 = []
+lowestRiskPath' c x y
+  | y >= (length c) = error "y too large"
+  | x >= (length $ head c) = error "x too large"
+  | otherwise = let w = length c
+                    h = length $ head c
+                    x' = w-x-1
+                    y' = h-y-1
+                    rightPath = case x-1 >= 0 of
+                                  True -> [c !! (x'+1) !! y'] ++ (fastLowestRiskPath c (x-1) y)
+                                  False -> [div maxBound 2]
+                    downPath = case y-1 >= 0 of
+                                 True -> [c !! x' !! (y'+1)] ++ (fastLowestRiskPath c x (y-1))
+                                 False -> [div maxBound 2]
+                 in case (sum downPath)<(sum rightPath) of
+                      True -> downPath
+                      False -> rightPath
+
 -- | Find the lowest-risk path through the cave
+-- [9]
+-- >>> lowestRiskPath ([9],[],[])
+-- [9]
 -- >>> lowestRiskPath ([],[9],[])
 -- [9]
 -- >>> lowestRiskPath ([6],[8],[[9]])
@@ -32,7 +82,7 @@ parseCave (x:xs) = let up = tail x
 lowestRiskPath :: Cave -> [Risk]
 lowestRiskPath (_,[x],[]) = [x]
 lowestRiskPath ([x],_,[]) = [x]
-lowestRiskPath (_,_,[[x]]) = [x]
+-- lowestRiskPath ([],[],[[x]]) = [x]
 -- lowestRiskPath cave@([],[],_) = []
 lowestRiskPath cave@(up,left,rest) = let rightPath = case up of
                                                        [] -> [div maxBound 2]
